3ccf794a98a25322c57fea7e63a952c1926c5615
napoli-lucia <lucianapoli15@gmail.com>
2024-02-01 20:23:14 -0300

    edit clase 5

---
+++ Clases/Clase05/express-router-p2-multer/src/utils.js
@@ -0,0 +1,25 @@
+const multer = require('multer') 
+const path = require('path') //libreria que maneja las rutas
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(`${__dirname}/public/uploads/`));
+    },
+    filename: function (req, file, cb) {
+      console.log("🚀 ~ file:", file);
+      cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+//cb: callback
+  
+const uploader = multer({
+storage,
+onError: function (err, next) {
+    console.log("🚀 ~ err:", err);
+    next();
+},
+});
+
+module.exports = {
+uploader,
+};
