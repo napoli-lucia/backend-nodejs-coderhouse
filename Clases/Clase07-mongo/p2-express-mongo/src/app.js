5e74be3995714e912961a3f03c071e98f64180b9
Lucia Napoli <lucianapoli15@gmail.com>
2024-02-26 14:57:45 -0300

    completo clase 7 con mongo

--- Clases/Clase07-mongo/p2-express-mongo/src/app.js
+++ Clases/Clase07-mongo/p2-express-mongo/src/app.js
@@ -1,15 +1,31 @@
 const express = require("express")
+const mongoose = require("mongoose");
 const studentsRoutes = require("./routes/students.routes")
 
 const app = express()
 const PORT = 5000;
 const API_PREFIX = "api";
 
+const DB_HOST = "localhost";
+const DB_PORT = 27017;
+const DB_NAME = "mongoStudentDB";
+
 app.use(express.urlencoded({ extends: true }));
 app.use(express.json());
 
 //http://localhost:5000/static/index.html
 app.use(`/static`, express.static(__dirname + "/public"));
+
+//El metodo connect es una promesa
+//Para operar una promesa tengo que usar then (resolve) y catch (reject)
+const connection = mongoose
+    .connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`)
+    .then((conn) => {
+      console.log("CONNECTED TO MONGO, WELCOME!!!");
+    })
+    .catch((err) => {
+      console.log("ERROR CONNECTION!!!", err);
+    });
 
 
 //STUDENTS ROUTES
--- Clases/Clase07-mongo/p2-express-mongo/src/routes/students.routes.js
+++ Clases/Clase07-mongo/p2-express-mongo/src/routes/students.routes.js
@@ -1,61 +1,162 @@
 const { Router } = require("express")
-//const listaUsuarios = require("../users.json");
+const studentsData = require("../data/students");
+const studentsModel = require("../model/students.model");
 
 const router = Router();
 
-// GET /api/users/
-router.get(`/`, (req, res) => {
-    return res.json({
-        ok: true,
-        usuarios: listaUsuarios.usuarios
-    })
-})
-
-// GET /api/users/:userId
-router.get(`/:userId`, (req, res) => {
-    console.log("PARAMS", req.params);
-    const userId = req.params.userId;
-
-    if (isNaN(userId)) {
-        return res.status(400).json({
-            ok: true,
-            message: `no existe el usuario con el id ${userId}`,
-            queryParams: req.query,
-        });
+// Ruta de insertion
+// GET /api/students/insertion
+router.get(`/insertion`, async (req, res) => {
+    try {
+      let result = await studentsModel.insertMany(studentsData);
+      return res.json({
+        message: "massive insert successfully",
+        students: result,
+      });
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: students.routes.js ~ router.get ~ error:",
+        error
+      );
     }
+  });
 
-    const usuario = listaUsuarios.usuarios.find((u) => {
-        return u.id === Number(userId);
-    });
-
-    if (!usuario) {
-        return res.json({
-            ok: true,
-            message: `no existe el usuario con el id ${userId}`,
-            usuario,
-            queryParams: req.query,
-        });
+// GET /api/students/
+router.get(`/`, async (req, res) => {
+    try {
+      let students = await studentsModel.find();
+      return res.json({
+        message: `student list`,
+        students,
+      });
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: students.routes.js ~ router.get ~ error:",
+        error
+      );
     }
+  });
 
-    return res.json({ ok: true, message: `usuario id: ${userId}`, usuario });
-});
+// GET /api/students/queries
+router.get(`/queries/age`, async (req, res) => {
+    try {
+      let studentsOlderAge = await studentsModel.find({
+        edad: { $gte: 30 },
+      });
+      return res.json({
+        message: `student list`,
+        studentsOlderAge
+      });
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: students.routes.js ~ router.get ~ error:",
+        error
+      );
+    }
+  });
 
-// POST /api/users/
-router.post(`/`, (req, res) => {
-    const user = req.body;
-    console.log("🚀 ~ router.post ~ user:", user);
-    const lastId = listaUsuarios.usuarios[listaUsuarios.usuarios.length - 1].id;
-    const newUser = {
-        id: lastId,
-        ...user,
-    };
-    res.json({ ok: true, message: `usuario creado`, usuario: newUser });
-});
+router.get(`/queries/v1`, async (req, res) => {
+    try {
+      let studentsOlderAge = await studentsModel.find({
+        edad: { $gte: 30 },
+      });
+  
+      let studentsApprovedOrYounger = await studentsModel.find({
+        $or: [{ nota: { $gte: 7 } }, { age: { $lt: 25 } }],
+      });
+  
+      let studentsBackend = await studentsModel.find({
+        curso: /^Backend$/i,
+        //Esto es una expresion regular
+        //Hace un match exacto
+      });
+  
+      let studentsEndReg = await studentsModel.find({
+        curso: { $regex: /end/i },
+        //regex (regular expresion)
+        //aca busca que contenga la expresion
+        //i de insensitive (puede ser mayuscula o minuscula)
+      });
+      return res.json({
+        message: `student list`,
+        studentsOlderAge,
+        studentsOlderAgeCount: studentsOlderAge.length,
+        studentsApprovedOrYounger,
+        studentsBackend,
+        studentsEndReg,
+      });
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: students.routes.js:63 ~ router.get ~ error:",
+        error
+      );
+    }
+  });
+  
 
-// PUT/api/users/:userId
-router.put(`/:userId`, (req, res) => { });
+router.get(`/queries/v2`, async (req, res) => {
+    try {
+      let studentsNoteWithProjection = await studentsModel.find(
+        {
+          nota: { $gte: 8 },
+        },
+        {
+          nombre: 1,
+          apellido: 1,
+          curso: 1,
+          nota: 1,
+        }
+      );
+  
+      let studentsNoteWithProjectionSorted = await studentsModel
+        .find(
+          {
+            nota: { $gte: 6 },
+          },
+          {
+            nombre: 1,
+            apellido: 1,
+            curso: 1,
+            nota: 1,
+          }
+        )
+        .sort({ nota: -1, edad: -1 });
+  
+      return res.json({
+        message: `student list`,
+        studentsNoteWithProjection,
+        studentsNoteWithProjectionSorted,
+      });
+    } catch (error) {}
+  });
+  
 
-// DELETE /api/users/:userId
-router.delete(`/:userId`, (req, res) => { });
+router.get(`/queries/v3`, async (req, res) => {
+    try {
+      let studentsSortedByAgeOlderToYounger = await studentsModel.find({}).sort({ edad: -1 });
+      //con -1 ordena de mayor a menor
+
+      let studentsSortedByAgeYoungerToOlder = await studentsModel.find({}).sort({ edad: 1 });
+  
+      let youngerStudent = await studentsModel
+        .find({})
+        .sort({ edad: 1 })
+        .limit(1);
+        //ordena de menor a mayor y agarra el primer elemento
+  
+      return res.json({
+        message: `student list`,
+        studentsSortedByAgeOlderToYounger,
+        studentsSortedByAgeYoungerToOlder,
+        youngerStudent,
+        // youngerStudent2,
+      });
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: students.routes.js ~ router.get ~ error:",
+        error
+      );
+    }
+  });
 
 module.exports = router;
